46d1b087adca75f5ad998fd723d13adce6aba3a5
romantisenko <105748122+RomanTischenko@users.noreply.github.com>
2023-04-25 15:42:16 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.kitchen__corousel').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/kitchen/arrow-left.png"></img></button>',
+        nextArrow: '<button tyle="button" class="slick-next"><img src="icons/kitchen/arrow-right.png"></img></button>'        
+    });
+});
